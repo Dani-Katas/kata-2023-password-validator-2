084090b80dd5733f2046ba407e8e178df4c19a1f
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-29 22:56:19 +0100

    implement a valid pasword test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,9 +1,15 @@
 import { describe, it, expect } from "vitest"
 import { myFunction } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+function validatePassword(validPassword: string): boolean {
+  return true
+}
+
+describe("validatePassword", () => {
+  it("returns true if the password meets all the requirements", () => {
+    const validPassword = "Aa1_xxxxx"
+
+    const result: boolean = validatePassword(validPassword)
 
     expect(result).toBe(true)
   })
