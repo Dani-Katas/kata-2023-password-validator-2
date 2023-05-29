b2c00f85960679dc7e83a02f885b8e8d9fde54e3
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-29 23:05:46 +0100

    refactor to class

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,27 +1,30 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { describe, expect, it } from "vitest"
 
-function validatePassword(password: string): boolean {
-  if (password.length <= 8) {
-    return false
+class PasswordValidator {
+  validate(password: string): boolean {
+    if (password.length <= 8) {
+      return false
+    }
+
+    if (password.toLowerCase() === password) {
+      return false
+    }
+
+    if (password.toUpperCase() === password) {
+      return false
+    }
+
+    return true
   }
-
-  if (password.toLowerCase() === password) {
-    return false
-  }
-
-  if (password.toUpperCase() === password) {
-    return false
-  }
-
-  return true
 }
 
 describe("validatePassword", () => {
+  const passwordValidator = new PasswordValidator()
+
   it("returns true if the password meets all the requirements", () => {
     const password = "Aa1_xxxxx"
 
-    const result: boolean = validatePassword(password)
+    const result: boolean = passwordValidator.validate(password)
 
     expect(result).toBe(true)
   })
@@ -30,7 +33,7 @@ describe("validatePassword", () => {
     it("if has less than 8 characters", () => {
       const password = "Aa1_xxxx"
 
-      const result: boolean = validatePassword(password)
+      const result: boolean = passwordValidator.validate(password)
 
       expect(result).toBe(false)
     })
@@ -38,7 +41,7 @@ describe("validatePassword", () => {
     it("if not has a capital letter", () => {
       const password = "aa1_xxxxx"
 
-      const result: boolean = validatePassword(password)
+      const result: boolean = passwordValidator.validate(password)
 
       expect(result).toBe(false)
     })
@@ -46,7 +49,7 @@ describe("validatePassword", () => {
     it("if not has a lowercase letter", () => {
       const password = "AA1_XXXXX"
 
-      const result: boolean = validatePassword(password)
+      const result: boolean = passwordValidator.validate(password)
 
       expect(result).toBe(false)
     })
