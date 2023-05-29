a1c99f52bd8816b0abae3bfc6f43302a9d6fcea6
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-29 22:57:16 +0100

    passing test for less than 9 characters

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,6 +2,10 @@ import { describe, it, expect } from "vitest"
 import { myFunction } from "./main.js"
 
 function validatePassword(validPassword: string): boolean {
+  if (validPassword.length <= 8) {
+    return false
+  }
+
   return true
 }
 
@@ -12,5 +16,13 @@ describe("validatePassword", () => {
     const result: boolean = validatePassword(validPassword)
 
     expect(result).toBe(true)
+  })
+
+  it("returns false if the password does not have more than 8 characters", () => {
+    const validPassword = "Aa1_xxxx"
+
+    const result: boolean = validatePassword(validPassword)
+
+    expect(result).toBe(false)
   })
 })
